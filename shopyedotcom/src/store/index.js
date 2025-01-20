4ef97e27359dd7e21018aa81a4888a0d6239898d
nadeemtadvi <nadeemtadvi1@gmail.com>
2025-01-20 13:10:07 +0530

    add cart functionality

--- shopyedotcom/src/store/index.js
+++ shopyedotcom/src/store/index.js
@@ -1,15 +1,7 @@
 import { combineReducers, createStore } from "redux";
-import cartReducer, {
-  cartAddItem,
-  cartaddItemQuantity,
-  cartRemoveItem,
-  cartRemoveItemQuantity,
-} from "./cartReducer";
+import cartReducer from "./cartReducer";
 import productReducer from "./productReducer";
-import wishlistReducer, {
-  wishlistAddItem,
-  wishlistRemoveItem,
-} from "./wishlistReducer";
+import wishlistReducer from "./wishlistReducer";
 
 const reducer = combineReducers({
   products: productReducer,
@@ -17,21 +9,9 @@ const reducer = combineReducers({
   wishLists: wishlistReducer,
 });
 
-export const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
+export const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__?.()
+);
 
 console.log(store);
-
-// store.dispatch(cartAddItem(1));
-// store.dispatch(cartAddItem(2));
-// store.dispatch(cartAddItem(3));
-// store.dispatch(cartAddItem(4));
-// store.dispatch(cartRemoveItem(4));
-// store.dispatch(cartaddItemQuantity(2));
-// store.dispatch(cartaddItemQuantity(2));
-// store.dispatch(cartRemoveItemQuantity(2));
-// store.dispatch(cartRemoveItemQuantity(2));
-// store.dispatch(cartRemoveItemQuantity(2));
-
-// store.dispatch(wishlistAddItem(3));
-// store.dispatch(wishlistAddItem(4));
-// store.dispatch(wishlistRemoveItem(4));
